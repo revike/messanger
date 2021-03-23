131435e012c6f94c44794f3039c84ed4839302c9
revike <revike@ya.ru>
2021-03-23 03:14:25 +0300

    hw_3 non_dec_db

--- hw_3/server_database.py
+++ hw_3/server_database.py
@@ -1,135 +1,137 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, create_engine, MetaData
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, DateTime
+from sqlalchemy.orm import mapper, sessionmaker
 
 from common.variables import SERVER_DATABASE
 
 
 class ServerDB:
-    Base = declarative_base()
 
-    class AllUsers(Base):
-        __tablename__ = 'all_users'
-        id = Column(Integer, primary_key=True)
-        login = Column(String, unique=True)
-        last_conn = Column(DateTime)
-
-        def __init__(self, login):
-            self.login = login
-            self.last_conn = datetime.now()
+    class AllUsers:
+        def __init__(self, username):
+            self.name = username
+            self.last_login = datetime.now()
             self.id = None
 
-    class ActiveUsers(Base):
-        __tablename__ = 'active_users'
-        id = Column(Integer, primary_key=True)
-        user = Column(String, ForeignKey('all_users.id'), unique=True)
-        ip = Column(String)
-        port = Column(Integer)
-        time_conn = Column(DateTime)
+    class ActiveUsers:
+        def __init__(self, user_id, ip_address, port, login_time):
+            self.user = user_id
+            self.ip_address = ip_address
+            self.port = port
+            self.login_time = login_time
+            self.id = None
 
-        def __init__(self, user, ip, port, time_conn):
-            self.user = user
+    class LoginHistory:
+        def __init__(self, name, date, ip, port):
+            self.id = None
+            self.name = name
+            self.date_time = date
             self.ip = ip
             self.port = port
-            self.time_conn = time_conn
-            self.id = None
-
-    class LoginHistory(Base):
-        __tablename__ = 'login_history'
-        id = Column(Integer, primary_key=True)
-        user = Column(String, ForeignKey('all_users.id'), unique=True)
-        ip = Column(String)
-        port = Column(Integer)
-        last_conn = Column(DateTime)
-
-        def __init__(self, user, ip, port, last_conn):
-            self.user = user
-            self.ip = ip
-            self.port = port
-            self.last_conn = last_conn
-            self.id = None
 
     def __init__(self):
-        self.engine = create_engine(SERVER_DATABASE, echo=False, pool_recycle=7200)
+        self.database_engine = create_engine(SERVER_DATABASE, echo=False, pool_recycle=7200)
+        self.metadata = MetaData()
 
-        self.Base.metadata.create_all(self.engine)
+        users_table = Table('Users', self.metadata,
+                            Column('id', Integer, primary_key=True),
+                            Column('name', String, unique=True),
+                            Column('last_login', DateTime)
+                            )
 
-        session = sessionmaker(bind=self.engine)
-        self.session = session()
+        active_users_table = Table('Active_users', self.metadata,
+                                   Column('id', Integer, primary_key=True),
+                                   Column('user', ForeignKey('Users.id'), unique=True),
+                                   Column('ip_address', String),
+                                   Column('port', Integer),
+                                   Column('login_time', DateTime)
+                                   )
+
+        user_login_history = Table('Login_history', self.metadata,
+                                   Column('id', Integer, primary_key=True),
+                                   Column('name', ForeignKey('Users.id')),
+                                   Column('date_time', DateTime),
+                                   Column('ip', String),
+                                   Column('port', String)
+                                   )
+
+        self.metadata.create_all(self.database_engine)
+
+        mapper(self.AllUsers, users_table)
+        mapper(self.ActiveUsers, active_users_table)
+        mapper(self.LoginHistory, user_login_history)
+
+        Session = sessionmaker(bind=self.database_engine)
+        self.session = Session()
 
         self.session.query(self.ActiveUsers).delete()
         self.session.commit()
 
-    def user_login(self, name, ip, port):
-        result = self.session.query(self.AllUsers).filter_by(login=name)
-        if result.count():
-            user = result.first()
-            user.last_conn = datetime.now()
+    def user_login(self, username, ip_address, port):
+
+        rez = self.session.query(self.AllUsers).filter_by(name=username)
+
+        if rez.count():
+            user = rez.first()
+            user.last_login = datetime.now()
         else:
-            user = self.AllUsers(name)
+            user = self.AllUsers(username)
             self.session.add(user)
             self.session.commit()
 
-        new_active_user = self.ActiveUsers(user.id, ip, port, datetime.now())
+        new_active_user = self.ActiveUsers(user.id, ip_address, port, datetime.now())
         self.session.add(new_active_user)
 
-        self.session.query(self.LoginHistory).filter_by(user=user.id).delete()
-        self.session.commit()
-
-        history = self.LoginHistory(user.id, ip, port, datetime.now())
+        history = self.LoginHistory(user.id, datetime.now(), ip_address, port)
         self.session.add(history)
 
         self.session.commit()
 
-    def user_logout(self, name):
-        user = self.session.query(self.AllUsers).filter_by(login=name).first()
+    def user_logout(self, username):
+        user = self.session.query(self.AllUsers).filter_by(name=username).first()
         self.session.query(self.ActiveUsers).filter_by(user=user.id).delete()
         self.session.commit()
 
     def users_list(self):
         query = self.session.query(
-            self.AllUsers.login,
-            self.AllUsers.last_conn,
+            self.AllUsers.name,
+            self.AllUsers.last_login,
         )
         return query.all()
 
     def active_users_list(self):
         query = self.session.query(
-            self.AllUsers.login,
-            self.ActiveUsers.ip,
+            self.AllUsers.name,
+            self.ActiveUsers.ip_address,
             self.ActiveUsers.port,
-            self.ActiveUsers.time_conn
-        ).join(self.AllUsers)
+            self.ActiveUsers.login_time
+            ).join(self.AllUsers)
         return query.all()
 
-    def login_history(self, name=None):
-        query = self.session.query(
-            self.AllUsers.login,
-            self.LoginHistory.last_conn,
-            self.LoginHistory.ip,
-            self.LoginHistory.port
-        ).join(self.AllUsers)
-
-        if name:
-            query = query.filter(self.AllUsers.login == name)
+    def login_history(self, username=None):
+        query = self.session.query(self.AllUsers.name,
+                                   self.LoginHistory.date_time,
+                                   self.LoginHistory.ip,
+                                   self.LoginHistory.port
+                                   ).join(self.AllUsers)
+        if username:
+            query = query.filter(self.AllUsers.name == username)
         return query.all()
 
 
 if __name__ == '__main__':
-    db = ServerDB()
-    db.user_login('client_1', '192.168.1.4', 8888)
-    # db.user_login('client_2', '192.168.1.5', 7777)
+    test_db = ServerDB()
+    # выполняем 'подключение' пользователя
+    test_db.user_login('client_1', '192.168.1.4', 8888)
+    test_db.user_login('client_2', '192.168.1.5', 7777)
     # выводим список кортежей - активных пользователей
-    print(db.active_users_list())
+    print(test_db.active_users_list())
     # выполянем 'отключение' пользователя
-    db.user_logout('client_1')
-    # db.user_logout('client_2')
-    # print(db.users_list())
+    test_db.user_logout('client_1')
     # выводим список активных пользователей
-    print(db.active_users_list())
+    print(test_db.active_users_list())
     # запрашиваем историю входов по пользователю
-    # print(db.login_history('client_2'))
-    # # выводим список известных пользователей
-    # print(db.users_list())
+    test_db.login_history('client_1')
+    # выводим список известных пользователей
+    print(test_db.users_list())
